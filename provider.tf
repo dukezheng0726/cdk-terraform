88dff05e307b664112c02fb9092bf5a0f9025093
dukezheng0726 <duke.zheng0726@gmail.com>
2025-02-11 22:02:34 -0500

    1

--- provider.tf
+++ provider.tf
@@ -7,6 +7,13 @@ terraform {
   }
 
   required_version = ">= 1.2.0"
+
+
+  backend "s3" {
+      bucket         = "yan-terraform"  # Replace with your S3 bucket name
+      key            = "terraform.tfstate"          # State file location within the bucket
+      region         = "var.region"                  # AWS region
+    }
 }
 
 provider "aws" {
